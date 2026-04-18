26b6410471f1086663232f84ff9986b69477785b
isi-code <irvinjose.silva@gmail.com>
2026-04-18 13:30:22 -0700

    Complete recursion exercise 1

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,8 @@
-const factorial = function() {
+const factorial = function(num) {
+    if (!Number.isInteger(num) || num < 0) return 
+    else if (num === 0) return 1
 
+    return num * factorial(num - 1)
 };
 
 // Do not edit below this line
