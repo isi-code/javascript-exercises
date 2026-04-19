b851df08293fe06ef49b32f5020f031b84791b9a
isi-code <irvinjose.silva@gmail.com>
2026-04-18 22:59:50 -0700

    Count number of integers in nested objects

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,6 +1,16 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+    if(typeof obj !== 'object' || obj === null) return 
+
+    return Object.values(obj).reduce((acc, val) => {
+      if (Number.isInteger(val)) return acc + 1;
+
+      else if(typeof val === "object" && val !== null) 
+        return acc + totalIntegers(val);
+
+      return acc
+    }, 0);
 };
   
+
 // Do not edit below this line
 module.exports = totalIntegers;
