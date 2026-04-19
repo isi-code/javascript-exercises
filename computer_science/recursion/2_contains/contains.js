d4b8de35897fd1e28df31ab75c5e6de79ede8049
isi-code <irvinjose.silva@gmail.com>
2026-04-18 22:58:26 -0700

    Find value in nested objects

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,18 @@
-const contains = function() {
-  
+const contains = function(obj, findV) {
+    const values = Object.values(obj);
+
+    if (values.includes(findV)) return true
+    const valObj = values.filter(val => { return typeof val === "object" && val !== null });
+
+    // if (valObj.length)
+    //     for (const val of valObj) 
+    //         if (contains(val, findV)) return true;
+    
+    // return false
+    
+    return valObj.some((nestedObject) =>
+    contains(nestedObject, findV)
+  );
 };
   
 // Do not edit below this line
