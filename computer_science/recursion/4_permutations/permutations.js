5ab6a4e5794a7ac8f4a37ebdcc5a1fe94d4f0173
isi-code <irvinjose.silva@gmail.com>
2026-05-20 19:20:16 -0700

    Add first approach to solve permutation problem
    I relied on randomness to get the expected results

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,6 +1,44 @@
-const permutations = function() {
-  
+const permutations = function(arr) {
+  if(!arr.length) return [arr]
+
+  const permNum = arr.reduce((acc,cur) => acc * cur );
+  const permArr = getPermutations([arr], permNum);
+
+  return permArr
 };
+
+function getPermutations(finalArr, permNum){
+    if(finalArr.length === permNum)
+        return finalArr
+
+    const ranArr = randomArr(finalArr[0]);
+    
+    const isInArray = (endArray, arr) => {
+        return endArray.some(
+            (elem) => elem.every((el, idx) => el === arr[idx] )
+        );
+    };
+
+    if(!isInArray(finalArr, ranArr))
+        finalArr.push(ranArr);
+
+    return getPermutations(finalArr, permNum)
+}
+
+function randomArr(arr){
+    const newArr = [];
+    let randomIdx, value;
+    
+    while(newArr.length !== arr.length){
+        randomIdx = Math.floor(Math.random() * arr.length);
+        value = arr[randomIdx];
+
+        if (!newArr.includes(value))
+            newArr.push(value);
+    }
+
+    return newArr
+}
   
 // Do not edit below this line
 module.exports = permutations;
