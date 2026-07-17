e3ed799c21489691a075c7061913f64504b27f7e
isi-code <irvinjose.silva@gmail.com>
2026-07-16 17:28:13 -0700

    Add first attempt to solve pascal problem
    5/7 passed tests

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,6 +1,16 @@
-const pascal = function() {
-  
+const pascal = function(rows, arr = [1]) {
+  if (rows === 1)
+    return arr
+
+  const newValuesArr = arr.map((num, idx) => {
+    const nextNum = (arr[idx+1] in arr) ? arr[idx+1] : 0;
+    return num + nextNum;
+  });
+
+  const result = [1, ...newValuesArr];
+
+  return pascal(rows-1, result)
 };
-  
+
 // Do not edit below this line
 module.exports = pascal;
--- computer_science/recursion/5_pascal/pascal.spec.js
+++ computer_science/recursion/5_pascal/pascal.spec.js
@@ -5,23 +5,23 @@ describe('pascal', () => {
     expect(pascal(1)).toEqual([1]);
   });
 
-  test.skip('Gets the second row of pascal', () => {
+  test('Gets the second row of pascal', () => {
     expect(pascal(2)).toEqual([1, 1]);
   });
 
-  test.skip('Gets the third row of pascal', () => {
+  test('Gets the third row of pascal', () => {
     expect(pascal(3)).toEqual([1, 2, 1]);
   });
 
-  test.skip('Gets the fourth row of pascal', () => {
+  test('Gets the fourth row of pascal', () => {
     expect(pascal(4)).toEqual([1, 3, 3, 1]);
   });
 
-  test.skip('Gets the fifth row of pascal', () => {
+  test('Gets the fifth row of pascal', () => {
     expect(pascal(5)).toEqual([1, 4, 6, 4, 1]);
   });
 
-  test.skip('Gets the sixth row of pascal', () => {
+  test('Gets the sixth row of pascal', () => {
     expect(pascal(6)).toEqual([1, 5, 10, 10, 5, 1]);
   });
 
