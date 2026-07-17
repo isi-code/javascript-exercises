2e0990b3c74d4afb9bbfad27eff086a53ad65bed
isi-code <irvinjose.silva@gmail.com>
2026-07-16 17:26:24 -0700

    Find permutations without relying on randomness

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,44 +1,16 @@
-const permutations = function(arr) {
-  if(!arr.length) return [arr]
-
-  const permNum = arr.reduce((acc,cur) => acc * cur );
-  const permArr = getPermutations([arr], permNum);
-
-  return permArr
-};
-
-function getPermutations(finalArr, permNum){
-    if(finalArr.length === permNum)
-        return finalArr
-
-    const ranArr = randomArr(finalArr[0]);
-    
-    const isInArray = (endArray, arr) => {
-        return endArray.some(
-            (elem) => elem.every((el, idx) => el === arr[idx] )
-        );
-    };
-
-    if(!isInArray(finalArr, ranArr))
-        finalArr.push(ranArr);
-
-    return getPermutations(finalArr, permNum)
-}
-
-function randomArr(arr){
-    const newArr = [];
-    let randomIdx, value;
-    
-    while(newArr.length !== arr.length){
-        randomIdx = Math.floor(Math.random() * arr.length);
-        value = arr[randomIdx];
-
-        if (!newArr.includes(value))
-            newArr.push(value);
-    }
-
-    return newArr
-}
+const permutations = function(array) {
+  if (array.length <= 1) return [array]
   
+  return array.reduce((result, num) => {
+    const arrs = array.filter(number => number !== num);
+    const subPermutations = permutations(arrs);
+
+    for (let arr of subPermutations)
+      result.push([num, ...arr])
+
+    return result
+  },[]);
+}
+
 // Do not edit below this line
 module.exports = permutations;
